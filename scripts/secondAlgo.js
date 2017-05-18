29cd20eb6720ef8fa5f93b35a652a4b93cd34bd8
Matt Pedersen <matthewnpedersen@gmail.com>
2017-05-17 21:50:57 -0700

    write skeleton for conditional for algo2

--- scripts/secondAlgo.js
+++ scripts/secondAlgo.js
@@ -8,31 +8,46 @@ var board = [
     [ 1, 1, 1, 2, 0, 0, 0 ]
 ]
 
-function checker(cords) {
+var lastPeice = [2, 0]
+var checkId = 2
+
+function checker(y, x) {
     console.log('checking from point', cords)
+
+    // going to check clockwise starting at 12:00
 
     // check up
     // [y - 1][x]
-    if (board[coords[0]])
+    if (board[y - 1] && board[y - 1][x] === checkId) {}
 
     // check up right
     // [y - 1][x + 1]
+    if (board[y - 1] && board[y - 1][x + 1] && board[y - 1][x + 1] === checkId) {}
 
     // check right
     // [y][x + 1]
+    if (board[y][x + 1] && board[y][x + 1] === checkId) {}
 
     // check down right
     // [y + 1][x + 1]
+    if (board[y + 1] && board[y + 1][x + 1] && board[y + 1][x + 1] === checkId) {}
 
     // check down
     // [y + 1][x]
+    if (board[y + 1] && board[y + 1][x] === checkId) {}
 
     // check down left
     // [y + 1][x - 1]
+    if (board[y + 1] && board[y + 1][x - 1] && board[y + 1][x - 1] === checkId) {}
 
     // check left
     // [y][x - 1]
+    if (board[y][x - 1] && board[y][x - 1] === checkId) {}
+
+    // check up left
+    // [y - 1][x - 1]
+    if (board[y - 1] && board[y - 1][x - 1] && board[y - 1][x - 1] === checkId)
 
 }
 
-checker([2, 0])
+checker(lastPeice[0], lastPeice[1])
