e9ad24973bd998a814a9722f838278f49ff4abdd
Matt Pedersen <matthewnpedersen@gmail.com>
2017-05-17 22:00:00 -0700

    before changing each conditional from checking if it exists and if it matches to only checking if it matches. makes it easier on the eyes

--- scripts/secondAlgo.js
+++ scripts/secondAlgo.js
@@ -9,16 +9,19 @@ var board = [
 ]
 
 var lastPeice = [2, 0]
+// var lastPeice = [5, 3]
 var checkId = 2
 
 function checker(y, x) {
-    console.log('checking from point', cords)
+    console.log('checking from point', [y, x])
+    var count = 1
 
     // going to check clockwise starting at 12:00
 
+    // NO NEED TO CHECK TOP, ANY PEICE DROPPED WILL AUTO BE AT THE VERY TOP, LOLOL
     // check up
     // [y - 1][x]
-    if (board[y - 1] && board[y - 1][x] === checkId) {}
+    // if (board[y - 1] && board[y - 1][x] === checkId) {}
 
     // check up right
     // [y - 1][x + 1]
@@ -30,7 +33,16 @@ function checker(y, x) {
 
     // check down right
     // [y + 1][x + 1]
-    if (board[y + 1] && board[y + 1][x + 1] && board[y + 1][x + 1] === checkId) {}
+    if (board[y + 1] && board[y + 1][x + 1] && board[y + 1][x + 1] === checkId) {
+        console.log('THE NEXT DOWN RIGHT IS A MATCH!')
+        for (var k = 1; k < 4; k++) {
+            if (board[y + k][x + k] === checkId) {
+                count++
+            } else {
+                count = 1
+            }
+        }
+    }
 
     // check down
     // [y + 1][x]
@@ -46,7 +58,17 @@ function checker(y, x) {
 
     // check up left
     // [y - 1][x - 1]
-    if (board[y - 1] && board[y - 1][x - 1] && board[y - 1][x - 1] === checkId)
+    if (board[y - 1] && board[y - 1][x - 1] && board[y - 1][x - 1] === checkId) {
+        console.log('THE NEXT UP LEFT IS A MATCH!')
+    }
+
+
+    // check the count
+    if (count === 4) {
+        console.log('VICTORY!!!')
+    } else {
+        console.log('LOSE!!!')
+    }
 
 }
 
