a3e36a5bf314b450c8636f0d69c06e2c7c66e6a3
Matt Pedersen <matthewnpedersen@gmail.com>
2017-05-17 22:53:25 -0700

    had to do the extensive check within the loop also. also needed to clear the connectArr if in the loop didnt match the connectId

--- scripts/secondAlgo.js
+++ scripts/secondAlgo.js
@@ -43,27 +43,27 @@
 // ]
 // var lastPeice = [2, 2]
 
-// DOWN LEFT VICTORY
-var board = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,2,0],
-    [0,0,0,0,2,1,0],
-    [1,0,0,2,2,2,0],
-    [1,0,2,1,1,1,0],
-]
-var lastPeice = [2, 5]
-
-// // LEFT VICTORY
+// // DOWN LEFT VICTORY
 // var board = [
 //     [0,0,0,0,0,0,0],
 //     [0,0,0,0,0,0,0],
 //     [0,0,0,0,0,2,0],
-//     [0,1,1,0,0,1,0],
-//     [2,2,2,2,0,1,0],
-//     [2,1,2,1,2,1,1],
+//     [0,0,0,0,2,1,0],
+//     [1,0,0,2,2,2,0],
+//     [1,0,2,1,1,1,0],
 // ]
-// var lastPeice = [4, 3]
+// var lastPeice = [2, 5]
+
+// LEFT VICTORY
+var board = [
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0],
+    [0,0,0,0,0,2,0],
+    [0,1,1,0,0,1,0],
+    [2,2,2,2,0,1,0],
+    [2,1,2,1,2,1,1],
+]
+var lastPeice = [4, 3]
 
 // // UP LEFT VICTORY
 // var board = [
@@ -92,7 +92,7 @@ function checker(y, x) {
     if (board[y - 1] && board[y - 1][x + 1] && board[y - 1][x + 1] === checkId) {
         console.log('THE NEXT UP RIGHT IS A MATCH!')
         for (var k = 1; k < 4; k++) {
-            if (board[y - k][x + k] === checkId) {
+            if (board[y - k] && board[y - k][x + k] && board[y - k][x + k] === checkId) {
                 connectArr.push([y - k, x + k])
                 count++
             } else {
@@ -106,10 +106,11 @@ function checker(y, x) {
     // [y][x + 1]
     if (board[y][x + 1] && board[y][x + 1] === checkId) {
         for (var k = 1; k < 4; k++) {
-            if (board[y][x + k] === checkId) {
+            if (board[y][x + k] && board[y][x + k] === checkId) {
                 connectArr.push([y, x + k])
                 count++
             } else {
+                connectArr = [ [y, x] ]
                 count = 1
                 break
             }
@@ -121,10 +122,11 @@ function checker(y, x) {
     if (board[y + 1] && board[y + 1][x + 1] && board[y + 1][x + 1] === checkId) {
         console.log('THE NEXT DOWN RIGHT IS A MATCH!')
         for (var k = 1; k < 4; k++) {
-            if (board[y + k][x + k] === checkId) {
+            if (board[y + k] && board[y + k][x + k] && board[y + k][x + k] === checkId) {
                 connectArr.push([y + k, x + k])
                 count++
             } else {
+                connectArr = [ [y, x] ]
                 count = 1
                 break
             }
@@ -136,10 +138,11 @@ function checker(y, x) {
     if (board[y + 1] && board[y + 1][x] === checkId) {
         console.log('THE NEXT DOWN IS A MATCH!')
         for (var k = 1; k < 4; k++) {
-            if (board[y + k][x] === checkId) {
+            if (board[y + k] && board[y + k][x] === checkId) {
                 connectArr.push([y + k, x])
                 count++
             } else {
+                connectArr = [ [y, x] ]
                 count = 1
                 break
             }
@@ -151,10 +154,11 @@ function checker(y, x) {
     if (board[y + 1] && board[y + 1][x - 1] && board[y + 1][x - 1] === checkId) {
         console.log('THE NEXT DOWN LEFT IS A MATCH!')
         for (var k = 1; k < 4; k++) {
-            if (board[y + k][x - k] === checkId) {
+            if (board[y + k] && board[y + k][x - k] && board[y + k][x - k] === checkId) {
                 connectArr.push([y + k, x - k])
                 count++
             } else {
+                connectArr = [ [y, x] ]
                 count = 1
                 break
             }
@@ -163,7 +167,19 @@ function checker(y, x) {
 
     // check left
     // [y][x - 1]
-    if (board[y][x - 1] && board[y][x - 1] === checkId) {}
+    if (board[y][x - 1] && board[y][x - 1] === checkId) {
+        console.log('THE NEXT DOWN RIGHT IS A MATCH!')
+        for (var k = 1; k < 4; k++) {
+            if (board[y][x - k] && board[y][x - k] === checkId) {
+                connectArr.push([y, x - k])
+                count++
+            } else {
+                connectArr = [ [y, x] ]
+                count = 1
+                break
+            }
+        }
+    }
 
     // check up left
     // [y - 1][x - 1]
