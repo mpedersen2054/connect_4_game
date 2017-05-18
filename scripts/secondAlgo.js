6432144c8e4174c7158213723e01bc637b198878
Matt Pedersen <matthewnpedersen@gmail.com>
2017-05-17 22:24:09 -0700

    had to revert bad to the multiple path checking, was throwing errors lol

--- scripts/secondAlgo.js
+++ scripts/secondAlgo.js
@@ -10,71 +10,71 @@ var board = [
 ]
 var lastPeice = [5, 2]
 
-// RIGHT VICTORY
-var board = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,2,0],
-    [0,1,1,0,0,1,0],
-    [2,2,2,2,0,1,0],
-    [2,1,2,1,2,1,1],
-]
-var lastPeice = [4, 0]
+// // RIGHT VICTORY
+// var board = [
+//     [0,0,0,0,0,0,0],
+//     [0,0,0,0,0,0,0],
+//     [0,0,0,0,0,2,0],
+//     [0,1,1,0,0,1,0],
+//     [2,2,2,2,0,1,0],
+//     [2,1,2,1,2,1,1],
+// ]
+// var lastPeice = [4, 0]
 
-// DOWN RIGHT VICTORY
-var board = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,2,0,0,0,0,0],
-    [0,1,2,0,0,0,0],
-    [0,2,1,2,0,0,0],
-    [1,1,1,2,2,0,1],
-]
-var lastPeice = [2, 1]
+// // DOWN RIGHT VICTORY
+// var board = [
+//     [0,0,0,0,0,0,0],
+//     [0,0,0,0,0,0,0],
+//     [0,2,0,0,0,0,0],
+//     [0,1,2,0,0,0,0],
+//     [0,2,1,2,0,0,0],
+//     [1,1,1,2,2,0,1],
+// ]
+// var lastPeice = [2, 1]
 
-// DOWN VICTORY
-var board = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,2,0,0,0,0],
-    [0,0,2,0,0,0,0],
-    [0,0,2,1,0,0,0],
-    [1,1,2,1,0,0,0],
-]
-var lastPeice = [2, 2]
+// // DOWN VICTORY
+// var board = [
+//     [0,0,0,0,0,0,0],
+//     [0,0,0,0,0,0,0],
+//     [0,0,2,0,0,0,0],
+//     [0,0,2,0,0,0,0],
+//     [0,0,2,1,0,0,0],
+//     [1,1,2,1,0,0,0],
+// ]
+// var lastPeice = [2, 2]
 
-// DOWN LEFT VICTORY
-var board = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,2,0],
-    [0,0,0,0,2,1,0],
-    [1,0,0,2,2,2,0],
-    [1,0,2,1,1,1,0],
-]
-var lastPeice = [2, 5]
+// // DOWN LEFT VICTORY
+// var board = [
+//     [0,0,0,0,0,0,0],
+//     [0,0,0,0,0,0,0],
+//     [0,0,0,0,0,2,0],
+//     [0,0,0,0,2,1,0],
+//     [1,0,0,2,2,2,0],
+//     [1,0,2,1,1,1,0],
+// ]
+// var lastPeice = [2, 5]
 
-// LEFT VICTORY
-var board = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,2,0],
-    [0,1,1,0,0,1,0],
-    [2,2,2,2,0,1,0],
-    [2,1,2,1,2,1,1],
-]
-var lastPeice = [4, 3]
+// // LEFT VICTORY
+// var board = [
+//     [0,0,0,0,0,0,0],
+//     [0,0,0,0,0,0,0],
+//     [0,0,0,0,0,2,0],
+//     [0,1,1,0,0,1,0],
+//     [2,2,2,2,0,1,0],
+//     [2,1,2,1,2,1,1],
+// ]
+// var lastPeice = [4, 3]
 
-// UP LEFT VICTORY
-var board = [
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,2,0,0,0,0,0],
-    [0,1,2,0,0,0,0],
-    [0,2,1,2,0,0,0],
-    [1,1,1,2,2,0,1],
-]
-var lastPeice = [5, 4]
+// // UP LEFT VICTORY
+// var board = [
+//     [0,0,0,0,0,0,0],
+//     [0,0,0,0,0,0,0],
+//     [0,2,0,0,0,0,0],
+//     [0,1,2,0,0,0,0],
+//     [0,2,1,2,0,0,0],
+//     [1,1,1,2,2,0,1],
+// ]
+// var lastPeice = [5, 4]
 
 
 
@@ -87,22 +87,19 @@ function checker(y, x) {
 
     // going to check clockwise starting at 12:00
 
-    // NO NEED TO CHECK TOP, ANY PEICE DROPPED WILL AUTO BE AT THE VERY TOP, LOLOL
-    // check up
-    // [y - 1][x]
-    // if (board[y - 1] && board[y - 1][x] === checkId) {}
-
     // check up right
     // [y - 1][x + 1]
-    if (board[y - 1][x + 1] === checkId) {}
+    if (board[y - 1] && board[y - 1][x + 1] && board[y - 1][x + 1] === checkId) {
+        console.log('THE NEXT UP RIGHT IS A MATCH!')
+    }
 
     // check right
     // [y][x + 1]
-    if (board[y][x + 1] === checkId) {}
+    if (board[y][x + 1] && board[y][x + 1] === checkId) {}
 
     // check down right
     // [y + 1][x + 1]
-    if (board[y + 1][x + 1] === checkId) {
+    if (board[y + 1] && board[y + 1][x + 1] && board[y + 1][x + 1] === checkId) {
         console.log('THE NEXT DOWN RIGHT IS A MATCH!')
         for (var k = 1; k < 4; k++) {
             if (board[y + k][x + k] === checkId) {
@@ -117,19 +114,19 @@ function checker(y, x) {
 
     // check down
     // [y + 1][x]
-    if (board[y + 1][x] === checkId) {}
+    if (board[y + 1] && board[y + 1][x] === checkId) {}
 
     // check down left
     // [y + 1][x - 1]
-    if (board[y + 1][x - 1] === checkId) {}
+    if (board[y + 1] && board[y + 1][x - 1] && board[y + 1][x - 1] === checkId) {}
 
     // check left
     // [y][x - 1]
-    if (board[y][x - 1] === checkId) {}
+    if (board[y][x - 1] && board[y][x - 1] === checkId) {}
 
     // check up left
     // [y - 1][x - 1]
-    if (board[y - 1][x - 1] === checkId) {
+    if (board[y - 1] && board[y - 1][x - 1] && board[y - 1][x - 1] === checkId) {
         console.log('THE NEXT UP LEFT IS A MATCH!')
     }
 
